77acd40d0c3dcbe53876489bc5f9e6e7d09e6d25
Ulises Lira <moredialiraulises13@gmail.com>
2023-09-08 14:53:01 -0600

    03 Operadores

---
+++ operadores.js
@@ -0,0 +1,36 @@
+// Operadores Matemáticos
+
+console.log(1 + 1);
+console.log(1 - 1);
+console.log(1 * 1);
+console.log(1 / 1);
+console.log(1 % 1);
+console.log(3 ** 3);
+
+// Más operadores
+let a = 10;
+
+console.log(a);
+a += 10; //a = a + 10
+console.log(a);
+a -= 10; //a = a - 10
+console.log(a);
+a *= 10; //a = a * 10
+console.log(a);
+a /= 10; //a = a / 10
+console.log(a);
+a %= 10; //a = a % 10
+console.log(a);
+
+// Operadores drelacionales
+console.log(10 < 10);
+console.log(10 <= 10);
+console.log(10 > 10);
+console.log(10 >= 10);
+console.log(10 != 10);
+console.log(10 === 10);
+
+// Operadores lógicos
+console.log(true && true);
+console.log(true || true);
+console.log(!true);
